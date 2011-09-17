8512afbb7e1b7701496cc3a581839f9a1e59fd51
jassinpain <jassinpain@gmail.com>
2011-09-17 01:04:59 -0700

    change in email address on server_inventory

--- server_inventory/metadata.rb
+++ server_inventory/metadata.rb
@@ -1,5 +1,5 @@
 maintainer       "Joshua SS Miller"
-maintainer_email "jassinpain@gmail.com"
+maintainer_email "cookbooks@fitsnps.net"
 license          "Apache License, Version 2.0"
 description      "Installs/Configures server_inventory"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
